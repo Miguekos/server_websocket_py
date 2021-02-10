2c4aa606248e2ced87d319029e4c636a10e0e528
Miguel Rodriguez <miguekos1233@gmail.com>
2021-02-09 21:03:41 -0500

    actualizando achicos docker - 1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from doctest import debug
 
 from flask import Flask, render_template, make_response, request, send_from_directory
-from flask_socketio import SocketIO, send, emit
+from flask_socketio import SocketIO, send, emit, Namespace
 from flask_cors import CORS
 from datetime import datetime
 import os
@@ -34,6 +34,32 @@ def handle_message(data):
     print(data)
     socketio.emit('message', data)
 
+
+class MyCustomNamespace(Namespace):
+    def on_connect(self):
+        print("cliente conectado")
+        pass
+
+    def on_disconnect(self):
+        print('Cliente desconectado')
+        pass
+
+    def on_my_event(self, data):
+        socketio.emit('message', data)
+        emit('my_response', data)
+
+    def on_message(self, data):
+        print(data)
+        socketio.emit('message', data)
+        emit('my_response', data)
+
+    @socketio.on('message')
+    def handle_message(data):
+        print(data)
+        socketio.emit('message', data)
+
+
 if __name__ == '__main__':
     # app.run(debug=True, port=5454, host="0.0.0.0")
+    socketio.on_namespace(MyCustomNamespace('/socketimpresora'))
     socketio.run(app, debug=True, port=7667, host="0.0.0.0")
